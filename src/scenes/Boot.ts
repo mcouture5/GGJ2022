c902d82dc359fb587b25462b336eb36e7a63d8a3
Mike <mikejcouture@gmail.com>
2022-01-29 16:31:10 -0500

    Fixed boot loading bar.

--- src/scenes/Boot.ts
+++ src/scenes/Boot.ts
@@ -1,5 +1,6 @@
 // VERY IMPORTANT TO KEEP THIS
 // It needs to be imported as a side effect so the plugin manager will run.
+import { BACKGROUND_HEX_COLOR } from '../constants';
 import '../plugins.ts';
 
 /**
@@ -9,6 +10,8 @@ export class Boot extends Phaser.Scene {
     private loadingBar: Phaser.GameObjects.Graphics;
     private progressBar: Phaser.GameObjects.Graphics;
     private camera: Phaser.Cameras.Scene2D.Camera;
+    private text: Phaser.GameObjects.Text;
+    private percent: number = 0;
 
     constructor() {
         super({
@@ -26,13 +29,15 @@ export class Boot extends Phaser.Scene {
 
         // pass value to change the loading bar fill
         this.load.on('progress', (value) => {
-            this.progressBar.clear();
-            this.progressBar.fillStyle(0x684949, 1);
+            this.percent = Math.floor(value * 100);
+            this.text.setText(`${this.percent}%`);
+            this.progressBar.fillStyle(BACKGROUND_HEX_COLOR, 1);
             this.progressBar.fillRect(this.camera.width / 4, this.camera.height / 2 - 16, (this.camera.width / 2) * value, 16);
         });
 
         // delete bar graphics, when loading complete
         this.load.on('complete', () => {
+            this.text.destroy();
             this.progressBar.destroy();
             this.loadingBar.destroy();
         });
@@ -57,5 +62,11 @@ export class Boot extends Phaser.Scene {
         this.loadingBar.fillStyle(0x684949, 1);
         this.loadingBar.fillRect(this.camera.width / 4 - 2, this.camera.height / 2 - 18, this.camera.width / 2 + 4, 20);
         this.progressBar = this.add.graphics();
+        
+        this.text = this.add.text(this.camera.width / 2, this.camera.height / 2 + 25, `${this.percent}%`, {
+            fontFamily: 'Octanis',
+            fontSize: '2rem',
+            color: '#000'
+        }).setOrigin(0.5, 0.5);
     }
 }
